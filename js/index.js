8604627d1fc478ed739bc25d4e62fe83b8c6b6b6
Procedeus <murilorez79@hotmail.com>
2022-08-08 14:43:41 -0300

    Atualização login

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const ul = document.querySelector("ul.squares");
+
+for(let i = 0; i < 11; i++){
+
+    const random = (min, max) => Math.random() * (max - min);
+
+    const li = document.createElement("li");
+    const position = random(1, 99);
+    const size = Math.floor(random(10, 40));
+    const delay = random(20, 4);
+    const duration = random(50, 25);
+    li.style.width = `${size}px`;
+    li.style.height = `${size}px`;
+    li.style.bottom = `-${size}px`;
+
+    li.style.left = `${position}%`;
+
+    li.style.animationDelay = `${delay}s`;
+    li.style.animationDuration = `${duration}s`;
+
+    ul.appendChild(li);
+}
